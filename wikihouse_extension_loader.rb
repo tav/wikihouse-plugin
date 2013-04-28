8eeae1393044fce64f7419210e532ec40d893aa4
Chris Musselle <chris.j.musselle@gmail.com>
2013-04-28 01:33:53 +0100

    Changed dimensions to a module vaiable. Added a Hash '@@wikihouse_settings' to store variable info.

--- wikihouse_extension_loader.rb
+++ wikihouse_extension_loader.rb
@@ -7,17 +7,21 @@ abs_lib_path = File.join(File.expand_path(File.dirname(__FILE__)), "/wikihouse-e
 $LOAD_PATH.unshift(abs_lib_path) unless $LOAD_PATH.include?(abs_lib_path)
 
 require 'utils.rb'
+require 'JSON.rb'
 
 module WikihouseExtension
 
   # Run Flags
-  WIKIHOUSE_DEV = true
-  WIKIHOUSE_LOCAL = false
-  WIKIHOUSE_HIDE = false
+  WIKIHOUSE_DEV = true   # If true brings up Ruby Console and loads some utility functions on startup
+  WIKIHOUSE_LOCAL = false 
+  WIKIHOUSE_HIDE = false  
   WIKIHOUSE_SHORT_CIRCUIT = false
 
   # Some Global Constants
   WIKIHOUSE_TITLE = 'Wikihouse' # name of Wikihouse project, incase it changes.
+  # Pannel stuff
+  PANEL_ID_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  PANEL_ID_ALPHABET_LENGTH = PANEL_ID_ALPHABET.length
   
   # Path setup 
   if WIKIHOUSE_LOCAL
@@ -44,9 +48,56 @@ module WikihouseExtension
     WIKIHOUSE_MAC = true
   end
   
+  # Set defaults for Global Variables 
+  
+  # Set Wikihouse Pannel Dimentions
+  wikihouse_sheet_height = 1200.mm
+  wikihouse_sheet_width = 2400.mm
+  wihihouse_panel_padding = 25.mm / 2
+  wikihouse_sheet_margin = 15.mm - wihihouse_panel_padding
+  wikihouse_font_height = 30.mm
+  wikihouse_sheet_inner_height = wikihouse_sheet_height - (2 * wikihouse_sheet_margin)
+  wikihouse_sheet_inner_width = wikihouse_sheet_width - (2 * wikihouse_sheet_margin)
+  
+  @@wikihouse_dimensions = [
+    wikihouse_sheet_height,
+    wikihouse_sheet_width,
+    wikihouse_sheet_inner_height,
+    wikihouse_sheet_inner_width,
+    wikihouse_sheet_margin,
+    wihihouse_panel_padding,
+    wikihouse_font_height
+  ]
+
+  #(Chris) Plan to eventually store all setting as a hash. 
+  #        Duplication exists for now to ensure code compatability.
+  
+  # Store the actual values as length objects (in inches)
+  @@wikihouse_settings = {
+  "sheet_height" => wikihouse_sheet_height,
+  "sheet_inner_height" => wikihouse_sheet_inner_height,
+  "sheet_width"  => wikihouse_sheet_width, 
+  "sheet_inner_width"  => wikihouse_sheet_inner_width, 
+  "padding"      => wihihouse_panel_padding,
+  "margin"       => wikihouse_sheet_margin,
+  "font_height"  => wikihouse_font_height,
+  }
+  
+  # Store default values for recall
+  DEFAULT_SETTINGS = Hash[@@wikihouse_settings]
+  
+  # Get and set methods for wikihouse_settings (so they can be returned via 
+  # referencing the module e.g.  WikihouseExtension.settings   )
+  def self.settings
+    @@wikihouse_settings
+  end
+  def self.settings=(settings)
+    @@wikihouse_settings = settings
+  end
+  
   # Define and Load the wikihouse Extension 
   WIKIHOUSE_EXTENSION = SketchupExtension.new "Wikihouse Plugin Development Version", "wikihouse-extension/wikihouse.rb"
-  WIKIHOUSE_EXTENSION.version = ' 0.1'
+  WIKIHOUSE_EXTENSION.version = ' 0.2 Dev'
   WIKIHOUSE_EXTENSION.description = "Allows for the sharing and downloading of wikihouse models at http://www.wikihouse.cc/, as well as the traslation of models to cutting templates."
   WIKIHOUSE_EXTENSION.creator = " Wikihouse Development Team"
   WIKIHOUSE_EXTENSION.copyright = " Public Domain - 2013"
